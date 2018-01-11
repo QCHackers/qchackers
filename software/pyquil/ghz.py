4f8c4a164c255b19073c405db83ed45a9e6129af
vtomole <vtomole@iastate.edu>
2018-01-10 20:22:59 -0600

    ghz efficient

--- software/pyquil/ghz.py
+++ software/pyquil/ghz.py
@@ -1,23 +1,15 @@
 from pyquil.quil import Program
 from pyquil.api import QPUConnection
+from pyquil.api import QVMConnection
 from pyquil.gates import *
 
 
-from pyquil.api import get_devices
-
-"""
-from pyquil.api import get_devices
-for device in get_devices():
-    if device.is_online():
-        print('Device {} is online'.format(device.name))
-
-"""
-
-qpu = QPUConnection(device_name='19Q-Acorn')
-
+#qpu = QPUConnection(device_name='19Q-Acorn')
+qvm = QVMConnection()
 ghz_state = Program(H(0), H(1), X(2), CNOT(1, 2), CNOT(0, 2), H(0), H(1), H(2))
-print("3Q GHZ State")
-result = qpu.run_and_measure(ghz_state, [0, 1, 2], 100)
+ghz_state_efficient = Program(H(0), CNOT(0, 1), CNOT(1, 2))
+print("3Q GHZ State QVM")
+result = qvm.run_and_measure(ghz_state, [0, 1, 2], 100)
 print(result)
 
 
