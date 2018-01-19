1e3ffe21f479c8e762d53c3dc805cba1c36f6502
vtomole <vtomole@iastate.edu>
2018-01-18 23:50:08 -0600

    add experiment file

---
+++ software/pyquil/experiments.py
@@ -0,0 +1,19 @@
+from pyquil.quil import Program
+#from pyquil.api import QPUConnection
+from pyquil.api import QVMConnection
+from pyquil.gates import *
+from numpy import pi
+
+#qpu = QPUConnection(device_name='19Q-Acorn')
+qvm = QVMConnection()
+exp = Program( RX(pi/2, 0), 
+        I(0),
+        RZ(-pi/2, 0),
+
+        RX(-pi/2, 0), 
+        I(0),
+        RZ(pi/2, 0))
+
+print("EXPERIMENT 1")
+result = qvm.run_and_measure(exp, [0], 10)
+print(result)
