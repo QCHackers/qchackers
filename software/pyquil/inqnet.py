327f48c134aa44ac2bd91ce3446b722fdd37bd14
vtomole <vtomole@iastate.edu>
2018-01-27 15:23:05 -0600

    inqnet

---
+++ software/pyquil/inqnet.py
@@ -0,0 +1,22 @@
+#https://www.reddit.com/r/QuantumComputing/comments/7tfl74/what_does_this_circuit_do/?st=jcxukv2c&sh=f19ff94e
+from pyquil.quil import Program
+from pyquil.api import QVMConnection
+from pyquil.gates import *
+
+qvm = QVMConnection()
+
+grover = Program(   
+    H(1),
+    CNOT(0, 1),
+    CNOT(1, 2),
+    CNOT(0, 1),
+    H(0),
+    CNOT(0, 1),
+    CZ(2, 0),
+    H(0),
+    H(0), 
+    
+)
+print("inqnet")
+result = qvm.run_and_measure(grover, [0, 1, 2], 5)
+print(result)
