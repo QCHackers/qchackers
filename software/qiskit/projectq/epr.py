cd5728addd4ecf028271faad338ee72bb7cea88d
vtomole <vtomole@iastate.edu>
2018-02-05 23:44:27 -0600

    epr projectq

---
+++ software/qiskit/projectq/epr.py
@@ -0,0 +1,11 @@
+from projectq import MainEngine  # import the main compiler engine
+from projectq.ops import H, Measure  # import the operations we want to perform (Hadamard and measurement)
+
+eng = MainEngine()  # create a default compiler (the back-end is a simulator)
+qubit = eng.allocate_qubit()  # allocate 1 qubit
+
+H | qubit  # apply a Hadamard gate
+Measure | qubit  # measure the qubit
+
+eng.flush()  # flush all gates (and execute measurements)
+print("Measured {}".format(int(qubit)))  # output measurement result
