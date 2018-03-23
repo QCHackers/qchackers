b4e4e19b4afaf669448f66592b1e2e1b28c02b0e
vtomole <vtomole@iastate.edu>
2018-03-22 22:51:45 -0500

    cleaned up the evaluator

--- software/eagle/vm.py
+++ software/eagle/vm.py
@@ -7,44 +7,49 @@ import itertools
 import re
 np.random.seed(int(time()))
 
+class Qubit:
+    "Define a qubit"
+    def __init__(self, address):
+        self.address = address
+        self.measurement = None
+
+class Gates:        
+    #pauli
+    I = np.matrix([[1, 0], [0, 1]])
+    X = np.matrix([[0, 1], [1, 0]])
+    Y = np.matrix([[0, 0 - 1j], [0 + 1j, 0]])
+    Z = np.matrix([[1, 0], [0, -1]])
+        
+    #universal gates
+    H = (X + Z)/np.sqrt(2)
+    T = np.matrix([[1, 0], [0, cmath.exp(1j * np.pi / 4)]])
+    S = np.matrix([[1.0, 0.0], [0.0, 1.0j]])
+    CNOT = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
+
+    #miscellaneous
+    CZ = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, -1, 0]])
+    SWAP = np.matrix([[1, 0, 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 0, 1]]) 
 
 class QuantumComputer:
     qregister = []
     cregister = [0,0,0,0,0]
+    qregister = []
     applied_gates = []
     ket_zero = np.matrix([[1], [0]])
     ket_one = np.matrix([[0], [1]])
+    wvf = ket_zero
+
+    def __init__(self, size):
+        for i in range(size):
+            self.qregister.append(Qubit(str(i)))
+
+            if size != 1:
+                self.wvf= np.kron(self.wvf, self.ket_zero)      
     
-    class Gates:        
-        #pauli
-        I = np.matrix([[1, 0], [0, 1]])
-        X = np.matrix([[0, 1], [1, 0]])
-        Y = np.matrix([[0, 0 - 1j], [0 + 1j, 0]])
-        Z = np.matrix([[1, 0], [0, -1]])
-        
-        #universal gates
-        H = (X + Z)/np.sqrt(2)
-        T = np.matrix([[1, 0], [0, cmath.exp(1j * np.pi / 4)]])
-        S = np.matrix([[1.0, 0.0], [0.0, 1.0j]])
-        CNOT = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-        CZ = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, -1, 0]])
-
-        SWAP = np.matrix([[1, 0, 0, 0],
-                          [0, 0, 1, 0],
-                          [0, 1, 0, 0],
-                          [0, 0, 0, 1]])
-
-    class Qubit:
-        "Define a qubit"
-        def __init__(self, address):
-            self.address = address
-            self.entangled = []
-            self.state = np.matrix([[1], [0]])
-            self.measurement = None
-            self.counter = 0
-            
-        def entangle(self, qubit):
-            self.entangled.append(qubit)
+       
             
 
 def two_n_size(wvf):
@@ -171,133 +176,57 @@ def pr(qubit, wvf, basis):
     return answer[0,0]
 
 
-def MEASURE(qubit, wvf):    
-    print("MEASURE", wavefunction(wvf), "\n")
-
+def MEASURE(qubit, wvf):
+    addr = qubit.address
     pr_zero = pr(qubit, wvf, 0)
     pr_one = pr(qubit, wvf, 1)
     
     sum =  + pr_zero + pr_one    
-    assert (round(sum) == 1.0),"Sum of probabilites does not equal"   
+    assert (round(sum) == 1.0),"Sum of probabilites does not equal 1"
 
-    perm_list = ["".join(seq) for seq in itertools.product("01", repeat=int(np.sqrt(len(qubit.state))))]
+    #print("MEASURE", wavefunction(wvf), "\n")
     
     if random.random() <= pr_zero:
         wvf = (proj(qubit, 0, wvf) * wvf)/(np.sqrt(pr_zero))
-        print("MEASUREMENT of qubit", qubit.address, "is" , 0, "\n")
+        qubit.measurement = 0
+        print("MEASUREMENT of qubit", addr, "is" , 0, "\n")
     else:
         wvf = (proj(qubit, 1, wvf) * wvf)/(np.sqrt(pr_one))
-        print("MEASUREMENT of qubit", qubit.address, "is" , 1, "\n")       
+        qubit.measurement = 1
+        print("MEASUREMENT of qubit", addr, "is" , 1, "\n")       
                 
     return wvf
 
 
-def init():
-    q0 = QuantumComputer.Qubit("0")
-    q1 = QuantumComputer.Qubit("1")
-    q2 = QuantumComputer.Qubit("2")
-    q3 = QuantumComputer.Qubit("3")   
-
-    QC.qregister.append(q0)
-    QC.qregister.append(q1)
-    QC.qregister.append(q2)
-    QC.qregister.append(q3)
-
-    QC.qregister[0].state =  np.matrix([[1], [0]])
-    QC.qregister[1].state =  np.matrix([[1], [0]])
-    QC.qregister[2].state =  np.matrix([[1], [0]])
-    QC.qregister[3].state =  np.matrix([[1], [0]])
-
+def evaluate(filepath):
+    global wv
     
-def evaluate(filepath):    
     with open(filepath) as fp:
         for line in fp:
             args = line.split()
             nArgs = len(args)
-            reset = 0
-            if nArgs == 1:
-                if args[0] == "init()":
-                    reset = 1
+            operator = args[0]
+        
             if nArgs == 2:
-                operator = args[0]
                 qubit = int(args[1])
+                if (operator == "MEASURE"):
+                    MEASURE(QC.qregister[int(qubit)], wv)
+                else:
+                    wv= apply_gate(QC.qregister[int(qubit)], wv, operator, "MISSING")
             elif nArgs == 3:
                 operator = args[0]
                 qubit = int(args[1])
                 qubit1 = int(args[2])
-                
-            if reset:
-                init()
-            elif(operator == "RX"):
-                RX(QC.qregister[int(qubit)])
-
-            elif(operator == "RY"):
-                RY(QC.qregister[int(qubit)])
-
-            elif(operator == "RZ"):
-                RZ(QC.qregister[int(qubit)])
-
-            elif(operator == "I"):
-                I(QC.qregister[int(qubit)])
-                
-            elif(operator == "X"):
-                X(QC.qregister[int(qubit)])
-
-            elif (operator == "Y"):
-                Y(QC.qregister[int(qubit)])
-
-            elif (operator == "Z"):
-                Z(QC.qregister[int(qubit)])
-
-            elif (operator == "H"):
-                H(QC.qregister[int(qubit)])
-
-            elif (operator == "T"):
-                T(QC.qregister[int(qubit)])
-                
-            elif (operator == "S"):
-                S(QC.qregister[int(qubit)])
-                
-            elif (operator == "CNOT"):
-                CNOT(QC.qregister[int(qubit)], QC.qregister[int(qubit1)])
-                
-            elif (operator == "MEASURE"):
-                MEASURE(QC.qregister[int(qubit)])               
+                wv= apply_gate(QC.qregister[int(qubit)], wv, operator, QC.qregister[int(qubit1)])
                 
             else:
-                print("Your operator does not exist")
-
-  
+                raise Exception("Exit(1)")
+            
     
 if __name__ == "__main__":
-    QC = QuantumComputer()
-    Gates = QC.Gates()
-    #init()
-    #evaluate(sys.argv[1])
-    q0 = QuantumComputer.Qubit("0")
-    q1 = QuantumComputer.Qubit("1")
-    q2 = QuantumComputer.Qubit("2")
-    q3 = QuantumComputer.Qubit("3")
-    state_zero = np.matrix([[1], [0]])
-    
-    wv= np.kron(state_zero, state_zero)
-    wv = np.kron (wv, state_zero)
-    wv = np.kron (wv, state_zero)
-
-    #setup
-    wv = apply_gate(q1, wv, "X")
-    wv = apply_gate(q0, wv, "H")    
-    wv = apply_gate(q1, wv, "H")
-
-    #Oracle
-    #wv = apply_gate(q0, wv, "CNOT", q1)
-    #wv = apply_gate(q1, wv, "X")
-    
-    #Finishing lap    
-    wv = apply_gate(q0, wv, "H")
-    wv = apply_gate(q1,wv, "H")
-    wv = MEASURE(q0, wv)
-    wv = MEASURE(q1, wv)
-
+    QC = QuantumComputer(4)
+    Gates = Gates()
+    wv = QC.wvf
+    evaluate(sys.argv[1])
     
     
