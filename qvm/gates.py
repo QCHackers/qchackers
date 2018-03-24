Fixing rotation gates, removing rounding

qvm/gates.py
@@ -1,16 +1,17 @@
 import numpy as np
-import cmath
 class Gates:
     "Contains basic quantum gates"
+    def round(self, x):
+        return np.around(x, decimals = 10)
 
     #Rotation
     def RX(self, theta):
-        return np.matrix([[round(np.cos(theta/ 2 )), np.around(-1j * np.sin(theta / 2))],
-                          [np.around(-1j * np.sin(theta / 2)), np.around(np.cos(theta/ 2 ))]])
+        return np.matrix([[np.cos(theta / 2 ), -1j * np.sin(theta / 2)],
+                          [-1j * np.sin(theta / 2), np.cos(theta/ 2 )]])
 
     def RY(self, theta):
-        return np.matrix([[np.cos(theta/ 2 ),  np.sin(theta / 2)],
-                          [-1j * np.sin(theta / 2), np.cos(theta/ 2 )]])
+        return np.matrix([[np.cos(theta / 2 ),  -np.sin(theta / 2)],
+                          [np.sin(theta / 2), np.cos(theta / 2 )]])
 
     def RZ(self, theta):
         return np.matrix([[np.exp(-1j * theta / 2),  0],
@@ -24,7 +25,7 @@ class Gates:
 
     #universal gates
     H = (X + Z)/np.sqrt(2)
-    T = np.matrix([[1, 0], [0, cmath.exp(1j * np.pi / 4)]])
+    T = np.matrix([[1, 0], [0, np.exp(1j * np.pi / 4)]])
     S = np.matrix([[1.0, 0.0], [0.0, 1.0j]])
     CNOT = np.matrix([[1, 0, 0, 0],
                       [0, 1, 0, 0],

qvm/tests/test_gates.py
@@ -1,7 +1,7 @@
 from gates import Gates
 import numpy as np
 
-def test_gate_matrices():
+def test_base_gate_matrices():
 
     #I X Y Z H T S  CNOT CZ SWAP
 
@@ -34,3 +34,14 @@ def test_gate_matrices():
 
     #SWAP
     assert np.isclose(Gates.SWAP, np.matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])).all()
+
+
+def test_rot_gate_matrices():
+    rx = Gates.RX(np.pi / 4)
+    assert np.isclose(rx, (np.cos(np.pi / 8) * np.eye(2)) - (1j * sin(np.pi / 8) * np.matrix([[0, 1], [1, 0]]))).all()
+
+    ry = Gates.RY(np.pi / 4)
+    assert np.isclose(ry, (np.cos(np.pi / 8) * np.eye(2)) - (1j * sin(np.pi / 8) * np.matrix([[0, -1j], [1j, 0]]))).all()
+
+    rz = Gates.RZ(np.pi / 4)
+    assert np.isclose(rz, (np.cos(np.pi / 8) * np.eye(2)) - (1j * sin(np.pi / 8) * np.matrix([[1, 0], [0, -1]]))).all()