33404449d6a4d6ca206ef857c8c866c0473d8349
Evan Anderson <ejdanderson@gmail.com>
2018-03-24 02:54:09 -0500

    Testig vm evaluation via string instead of file

--- program.py
+++ program.py
@@ -5,9 +5,9 @@ class Program:
     def __init__(self, instructions):
         vm.evaluate(instructions, "string")
 
-
-    
-
-
- 
-    
+if __name__ == "__main__":
+    p = """H 0
+    CNOT 0 1
+    MEASURE 1
+    MEASURE 0"""
+    Program(p)
