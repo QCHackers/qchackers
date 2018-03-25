30488828b168e839c69d94737371eb647b339fba
vtomole <vtomole@iastate.edu>
2018-03-24 19:54:34 -0500

    pretty print results on api

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 
+import requests
+
 url ='https://whispering-mountain-51676.herokuapp.com//api/add_message/1234'
 
 with open('programs/epr.eg', 'r') as myfile:
-    data=myfile.read()
+    data=myfile.read() 
 
-print(data)
-    
-import requests
 res = requests.post(url, json={"mytext":data})
+
 if res.ok:
-    print (res.json())
+    j = res.json()
+    print (j["results"])
