eb23f0cc35b3edd8f0ebb06b9ce69f2e4f897a77
vtomole <vtomole@iastate.edu>
2018-03-25 02:11:12 -0500

    inde.html init

--- app.py
+++ app.py
@@ -7,14 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
+    return render_template('index.html')
+    
 
 @app.route('/tic-tac-toe')
 def tic_tac_toe():
