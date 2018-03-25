37193d81ec8e527c2a5d7c04af53bd8417fd92db
vtomole <vtomole@iastate.edu>
2018-03-25 01:51:12 -0500

    return cat homepage

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+
+@app.route('/tic-tac-toe')
+def homepage():
     return render_template('index.html')
 
 
