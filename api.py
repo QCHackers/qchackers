cd26b8ad284731938d7042cac02b5ce971474f8d
vtomole <vtomole@iastate.edu>
2018-03-24 19:57:32 -0500

    Can run from file

--- api.py
+++ api.py
@@ -1,13 +1,18 @@
 
+import sys
 import requests
 
 url ='https://whispering-mountain-51676.herokuapp.com//api/add_message/1234'
 
-with open('programs/epr.eg', 'r') as myfile:
-    data=myfile.read() 
+def run(path):    
+    with open(path, 'r') as myfile:
+        data=myfile.read() 
+        res = requests.post(url, json={"mytext":data})
+    
+        if res.ok:
+            j = res.json()
+            print (j["results"])
 
-res = requests.post(url, json={"mytext":data})
 
-if res.ok:
-    j = res.json()
-    print (j["results"])
+if __name__ == "__main__":    
+    run(sys.argv[1])
