7b1a937c5d449e5b329634a782cee55784077102
vtomole <vtomole@iastate.edu>
2018-03-27 12:39:02 -0500

    remove move

--- app.py
+++ app.py
@@ -39,40 +39,6 @@ def add_message(uuid):
     return jsonify({"results" : msg})
 
 
-@app.route('/move', methods=['POST'])
-def move():
-    post = request.get_json()
-
-    game = Game()
-    game.board = post.get('board')
-    game.player = post.get('player')
-    game.computer = post.get('computer')
-
-    # Check if player won
-    if game.has_won(game.player):
-        return jsonify(tied = False, computer_wins = False, player_wins = True, board = game.board)
-    elif game.is_board_full():
-        return jsonify(tied = True, computer_wins = False, player_wins = False, board = game.board)
-
-    # Calculate computer move
-    computer_move = game.calculate_move()
-
-    # Make the next move
-    game.make_computer_move(computer_move['row'], computer_move['col'])
-
-    # Check if computer won
-    if game.has_won(game.computer):
-        return jsonify(computer_row = computer_move['row'], computer_col = computer_move['col'],
-                       computer_wins = True, player_wins = False, tied=False, board = game.board)
-    # Check if game is over
-    elif game.is_board_full():
-        return jsonify(computer_row = computer_move['row'], computer_col = computer_move['col'],
-                       computer_wins = False, player_wins = False, tied=True, board=game.board)
-
-    # Game still going
-    return jsonify(computer_row = computer_move['row'], computer_col = computer_move['col'],
-                   computer_wins = False, player_wins = False, board = game.board)
-
 
 @app.route('/api/teleportsend', methods=['get', 'post'])
 def teleportsend():
