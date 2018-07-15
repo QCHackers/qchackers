531864a8898d7207436b4055ce2892e650832dfc
vtomole <vtomole@iastate.edu>
2018-07-15 15:36:44 -0500

    qram

--- pyquil/qram.py
+++ pyquil/qram.py
@@ -5,14 +5,44 @@ from pyquil.gates import *
 
 
 #qpu = QPUConnection(device_name='19Q-Acorn')
-qvm = QVMConnection()
+qvm = QVMConnection(use_queue=True)
 
 
 # f0
 qram = Program(
-    #Put qubit 0 and qubit 1 into superposition
+   
+    X(10),    
+    CNOT(2,6),
+    CNOT(6,10),
+    CCNOT(6,4,5),
+    CCNOT(1,6,4),
+    CCNOT(1,10,8),
+    CNOT(8,10),
+    CNOT(4,6),
+    CCNOT(0,4,3),
+    CCNOT(0,6,5),
+    CCNOT(0,8,7),
+    CCNOT(0,10,9),
+    CNOT(9,10),
+    CNOT(7,8),
+    CNOT(5,6),
+    CNOT(3,4),
+    CCNOT(10,18,19),
+    CCNOT(9,17,19),
+    CCNOT(8,16,19),
+    CCNOT(7,15,19),
+    CCNOT(6,14,19),
+    CCNOT(5,13,19),
+    CCNOT(4,12,19),
+    CCNOT(3,11,19),
+        
 )
 
-#print("Deutsch QVM N = 2 constant")
-#result = qvm.run_and_measure(deutsch, [0], 5)
-#print(result)
+    
+   
+    
+
+
+print(qram.instructions())
+#result = qvm.run_and_measure(qram, [10,18,19])
+#print("Expected 1 " , result)
---
+++ pyquil/teleportation.py
@@ -0,0 +1,33 @@
+from pyquil.quil import Program
+#from pyquil.api import QPUConnection
+from pyquil.api import QVMConnection
+from pyquil.gates import *
+
+qvm = QVMConnection()
+
+ins = Program()
+
+ins.inst(H(1), CNOT(1,2))  #Creating B00
+ins.inst(CNOT(0,1),H(0))
+ins.measure(0,0).measure(1,1).if_then(1, X(2)).if_then(0, Z(2))
+wvf = qvm.wavefunction(ins, [0,1])
+#print( wvf)
+
+
+ins = Program(
+    H(0),
+    H(1),
+    CNOT(1,2),
+    CNOT(0,1),
+    H(0),
+    )
+
+
+ins.measure(0,0).measure(1,1).if_then(1, X(2)).if_then(1, Z(2))
+wvf = qvm.wavefunction(ins)
+
+print(ins)
+result = qvm.run_and_measure(ins, [2])
+print(result)
+
+print(wvf)
